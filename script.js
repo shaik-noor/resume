36824861e74354e8599ccbd57bc1354dcf834876
Shaik_Noor <s.noorink@gmail.com>
2023-07-22 17:52:48 +0530

    adding the dark mode

--- script.js
+++ script.js
@@ -21,6 +21,11 @@ document.addEventListener("DOMContentLoaded", function() {
   listContainer.style.display = "none";
 });
 
+// dark mode toggle
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
+
 
 //testing new carousel start wiht touch
 
@@ -141,81 +146,6 @@ certificateSlides.forEach(slide => {
   slide.addEventListener('touchend', resumeAutoLoop);
 });
 
-//testing new carousel end
-
-// const carousel = document.querySelector('.carousel');
-// const prevBtn = document.querySelector('.prev-btn');
-// const nextBtn = document.querySelector('.next-btn');
-// const certificateSlides = document.querySelectorAll('.certificate-slide');
-// const slideIndicatorsContainer = document.querySelector('.slide-indicators');
-
-// let currentIndex = 0;
-// const slideWidth = certificateSlides[0].offsetWidth;
-
-// function showSlide(index) {
-//   carousel.style.transform = `translateX(-${index * slideWidth}px)`;
-//   updateSlideIndicators(index);
-// }
-
-// function showNextSlide() {
-//   currentIndex++;
-//   if (currentIndex >= certificateSlides.length) {
-//     currentIndex = 0;
-//   }
-//   showSlide(currentIndex);
-// }
-
-// function showPrevSlide() {
-//   currentIndex--;
-//   if (currentIndex < 0) {
-//     currentIndex = certificateSlides.length - 1;
-//   }
-//   showSlide(currentIndex);
-// }
-
-// function updateSlideIndicators(currentIndex) {
-//   const slideIndicators = document.querySelectorAll('.slide-indicator');
-//   slideIndicators.forEach((indicator, index) => {
-//     if (index === currentIndex) {
-//       indicator.classList.add('active');
-//     } else {
-//       indicator.classList.remove('active');
-//     }
-//   });
-// }
-
-// nextBtn.addEventListener('click', showNextSlide);
-// prevBtn.addEventListener('click', showPrevSlide);
-
-// // Auto loop the carousel
-// let intervalId;
-
-// function startAutoLoop() {
-//   intervalId = setInterval(showNextSlide, 3000);
-// }
-
-// function stopAutoLoop() {
-//   clearInterval(intervalId);
-// }
-
-// startAutoLoop();
-
-// // Pause auto loop on hover
-// carousel.addEventListener('mouseenter', stopAutoLoop);
-// carousel.addEventListener('mouseleave', startAutoLoop);
-
-// // Create slide indicators and add event listeners
-// certificateSlides.forEach((_, index) => {
-//   const indicator = document.createElement('div');
-//   indicator.classList.add('slide-indicator');
-//   indicator.addEventListener('click', () => showSlide(index));
-//   slideIndicatorsContainer.appendChild(indicator);
-// });
-
-// // Show the initial slide indicator
-// updateSlideIndicators(currentIndex);
-
-
   document.addEventListener("DOMContentLoaded", function () {
     const desktopNav = document.getElementById("desktop-nav");
     const hamburgerNav = document.getElementById("hamburger-nav");
@@ -263,26 +193,30 @@ certificateSlides.forEach(slide => {
   });
   
 
-  // work title typing effect 
-
   const stringsToType = [
-    { text: "Technical Support Engineer", delay: 100 },
-    { text: "At Informatica", delay: 80, color: "#FF4D00"},
+    { text: "Technical Support Engineer", delay: 100,},
+    { text: "At Informatica", delay: 80, color: "#FF4D00" },
   ];
-
+  
   const cursorBlinkDelay = 500; // Milliseconds for the cursor to blink
-
+  
   const targetElement = document.getElementById("typingEffect");
   let stringIndex = 0;
   let charIndex = 0;
-
+  
   function typeNextCharacter() {
     const currentString = stringsToType[stringIndex].text;
     const currentDelay = stringsToType[stringIndex].delay || 100;
     const currentColor = stringsToType[stringIndex].color || "#000"; // Default to black color if not specified
-
+  
     if (charIndex < currentString.length) {
-      targetElement.style.color = currentColor; // Set the text color
+      if (document.body.classList.contains("dark-mode") && currentString === "Technical Support Engineer") {
+        // Check if the dark mode class is present on the body and if the current string is "Technical Support Engineer"
+        targetElement.style.color = "#FFF"; // Set the text color to white in dark mode only for "Technical Support Engineer"
+      } else {
+        targetElement.style.color = currentColor; // Set the text color to the specified color in normal mode or for other strings in dark mode
+      }
+  
       targetElement.innerHTML += currentString.charAt(charIndex);
       charIndex++;
       setTimeout(typeNextCharacter, currentDelay);
@@ -294,17 +228,17 @@ certificateSlides.forEach(slide => {
       setTimeout(clearTextAndTypeNext, 1000);
     }
   }
-
+  
   function clearTextAndTypeNext() {
     targetElement.innerHTML = '';
     typeNextCharacter();
   }
-
+  
   function cursorBlink() {
     targetElement.style.borderRightColor = targetElement.style.borderRightColor === "transparent" ? "#000" : "transparent";
     setTimeout(cursorBlink, cursorBlinkDelay);
   }
-
+  
   // Start the typing effect when the page loads
   window.onload = () => {
     typeNextCharacter();
