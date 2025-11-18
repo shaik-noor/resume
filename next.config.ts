5f9ffdf8de456564553556b19b2cd672026a75d3
Shaik Noor <s.noorink@gmail.com>
2025-11-18 23:11:15 +0530

    chore: update dependencies and remove unused experimental config
    - Remove unused experimental lightningcss config from next.config.ts
    - Update @types/node, @types/react, @types/react-dom, react-hook-form, and other dependencies
    - Add @emnapi/runtime as an optional dependency

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	output: "standalone",
-	experimental: {
-	useLightningcss: false,
-    // reactCompiler: true,
-	}
+	
 };
 
 export default nextConfig;
