32f8dce7f7db27709dc8bed92e0f7055cf203bac
Shaik Noor <s.noorink@gmail.com>
2025-11-18 23:06:15 +0530

    build(next): disable lightningcss experimental feature
    The lightningcss feature is still experimental and causing build issues in our environment. Disabling it for now until it's more stable.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	output: "standalone",
+	experimental: {
+	useLightningcss: false,
+    // reactCompiler: true,
+	}
 };
 
 export default nextConfig;
